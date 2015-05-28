15a4c2ac1c820896b082c5cc3dc9988a23d38b28
Brancho <branka.martinovic@live.com>
2015-05-28 13:09:16 +0200

    angular ui bootstrap

--- tnation.js
+++ tnation.js
@@ -1,22 +1,25 @@
 
-var app = angular.module('test', []);
+var app = angular.module('test', ['ui.bootstrap']);
 
     app.controller('submitController', ['$scope','$http', function($scope, $http) {
-      $scope.list = [];
-      $scope.text = '';
-      $scope.submit = function() {
-        if ($scope.text) {
-          $scope.list.push(this.text);
-          $scope.text = '';
-        }
-        $scope.remove = function($index) { 
-        $scope.list.splice($index, 1);     
-      }
-      };
 
       $http.get('podaci.json').success(function(data) {
       $scope.data = data;
   });
+
+      $scope.answers = [];
+      $scope.text = '';
+      $scope.submit = function() {
+        if ($scope.text) {
+          $scope.answers.push(this.text);
+          $scope.text = '';
+        }
+        $scope.remove = function($index) { 
+        $scope.answers.splice($index, 1);     
+      }
+      };
+
+
 
     }]);
 
